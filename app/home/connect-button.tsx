023070f4366d038368dcf20dfbe30aad9e5dca73
JamesHealyi <50569110+JamesHealyi@users.noreply.github.com>
2025-01-07 18:24:23 +0800

    feat: refactor connect button

--- app/home/connect-button.tsx
+++ app/home/connect-button.tsx
@@ -7,10 +7,10 @@ import {
   Spin,
 } from "@/primitive/components";
 import { useWallet, Wallet } from "@solana/wallet-adapter-react";
-import { WalletReadyState } from "@solana/wallet-adapter-base";
+import { WalletName, WalletReadyState } from "@solana/wallet-adapter-base";
 import { useMemoizedFn } from "ahooks";
 import Image from "next/image";
-import { useEffect, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import { Address } from "@/components/address";
 
 export function ConnectButton() {
@@ -43,6 +43,15 @@ export function ConnectButton() {
       });
     }
   }, [wallet]);
+  const buttonText = useMemo(() => {
+    if (publicKey) {
+      return <Address address={publicKey.toBase58()} enableCopy />;
+    } else if (connecting) {
+      return "Connecting";
+    } else {
+      return "Connect Wallet";
+    }
+  }, [connecting, publicKey]);
   return (
     <>
       <Button
@@ -50,50 +59,68 @@ export function ConnectButton() {
         className='!w-[400px] !bg-white !text-black'
         size='l'
       >
-        {publicKey ? (
-          <Address address={publicKey.toBase58()} enableCopy />
-        ) : connecting ? (
-          "Connecting"
-        ) : (
-          "Connect Wallet"
-        )}
+        {buttonText}
       </Button>
       <Modal open={visible} onMaskClick={onClose} size='s'>
         <ModalTitleWithBorder>Connect Wallet</ModalTitleWithBorder>
         <ModalContent className='gap-8'>
           {wallets.map((w: Wallet) => (
-            <button
+            <WalletButton
               key={w.adapter.name}
-              onClick={() => {
-                select(w.adapter.name);
-              }}
-              className='h-48 rounded-8 p-12 hover:bg-surface flex items-center justify-between group'
-            >
-              <div className='flex items-center gap-8'>
-                <Image
-                  src={w.adapter.icon}
-                  height={32}
-                  width={32}
-                  className='rounded-full'
-                  alt={w.adapter.name}
-                />
-                <p className='text-size-16 font-semibold'>{w.adapter.name}</p>
-              </div>
-              {wallet?.adapter.name === w.adapter.name && connecting ? (
-                <Spin className='text-size-16' />
-              ) : w.readyState === WalletReadyState.Installed ? (
-                "Detected"
-              ) : w.readyState === WalletReadyState.NotDetected ? (
-                <a href={w.adapter.url} target='_blank'>
-                  Download
-                </a>
-              ) : (
-                ""
-              )}
-            </button>
+              loading={connecting && wallet?.adapter.name === w.adapter.name}
+              select={select}
+              wallet={w}
+            />
           ))}
         </ModalContent>
       </Modal>
     </>
+  );
+}
+
+function WalletButton({
+  wallet,
+  loading,
+  select,
+}: {
+  wallet: Wallet;
+  loading: boolean;
+  select: (walletName: WalletName | null) => void;
+}) {
+  const suffix = useMemo(() => {
+    if (loading) {
+      return <Spin className='text-size-16' />;
+    } else if (wallet.readyState === WalletReadyState.Installed) {
+      return "Detected";
+    } else if (wallet.readyState === WalletReadyState.NotDetected) {
+      return (
+        <a href={wallet.adapter.url} target='_blank'>
+          Download
+        </a>
+      );
+    } else {
+      return "";
+    }
+  }, [loading, wallet.readyState, wallet.adapter.url]);
+  return (
+    <button
+      key={wallet.adapter.name}
+      onClick={() => {
+        select(wallet.adapter.name);
+      }}
+      className='h-48 rounded-8 p-12 hover:bg-surface flex items-center justify-between group'
+    >
+      <div className='flex items-center gap-8'>
+        <Image
+          src={wallet.adapter.icon}
+          height={32}
+          width={32}
+          className='rounded-full'
+          alt={wallet.adapter.name}
+        />
+        <p className='text-size-16 font-semibold'>{wallet.adapter.name}</p>
+      </div>
+      {suffix}
+    </button>
   );
 }

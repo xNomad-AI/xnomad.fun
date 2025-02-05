6274d1efde466736746e1b9d77f2784e836a8be3
JamesHealyi <healthonyi@gmail.com>
2025-02-05 20:20:37 +0800

    feat: persist chat history

--- app/layout/wallet-provider.tsx
+++ app/layout/wallet-provider.tsx
@@ -8,7 +8,23 @@ import { clusterApiUrl } from "@solana/web3.js";
 import "@solana/wallet-adapter-react-ui/styles.css";
 import { PropsWithChildren, useMemo } from "react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-const queryClient = new QueryClient();
+import { persistQueryClient } from "@tanstack/react-query-persist-client";
+import { createSyncStoragePersister } from "@tanstack/query-sync-storage-persister";
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: Infinity,
+    },
+  },
+});
+
+persistQueryClient({
+  queryClient: queryClient,
+  persister: createSyncStoragePersister({
+    storage: window.localStorage,
+  }),
+  maxAge: Infinity,
+});
 export function WalletProvider({ children }: PropsWithChildren<object>) {
   const endpoint = clusterApiUrl(
     process.env.DEPLOY_ENV === "prod" ? "mainnet-beta" : "devnet"

a1fcd360385350e078ae021b6f08dea5ac0fd976
JamesHealyi <healthonyi@gmail.com>
2025-02-09 00:02:16 +0800

    feat: check airdrop status

--- network/use-airdrops.ts
+++ network/use-airdrops.ts
@@ -1,6 +1,8 @@
+import { userStorage } from "@/lib/user/storage";
 import { api } from "@/primitive/api";
+import { useMemoizedFn } from "ahooks";
 import { useEffect, useState } from "react";
-export interface Airdrop {
+interface _Airdrop {
   id: string;
   protocol: "AirdropProgramRegistry";
   version: string;
@@ -36,10 +38,46 @@ export interface Airdrop {
     estimateCost: string | null;
     createdAt: string;
     updatedAt: string;
+    checkEligibilityUrl: string;
   };
 }
-export function useAirdrops({ name }: { name?: string }) {
+interface ClaimStatus {
+  claimable?: boolean;
+  claimed?: boolean;
+}
+export type Airdrop = _Airdrop & ClaimStatus;
+export function useAirdrops({
+  name,
+  agentAddress,
+}: {
+  name?: string;
+  agentAddress: string;
+}) {
   const [airdrops, setAirDrops] = useState<Airdrop[]>([]);
+  const checkStatus = useMemoizedFn(async (airdrops: Airdrop[]) => {
+    const processedAirdrops = [];
+    for (const airdrop of airdrops) {
+      try {
+        const res = await fetch(airdrop.rules.checkEligibilityUrl, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${userStorage.getCurrentToken()?.jwt}`,
+          },
+          body: JSON.stringify({
+            agentAddress: agentAddress,
+          }),
+        });
+        const { claimable, claimed }: ClaimStatus = await res.json();
+        airdrop.claimable = claimable;
+        airdrop.claimed = claimed;
+        processedAirdrops.push(airdrop);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    setAirDrops(processedAirdrops);
+  });
   useEffect(() => {
     api.airdrop
       .get<Airdrop[]>("/registry", {
@@ -47,6 +85,7 @@ export function useAirdrops({ name }: { name?: string }) {
       })
       .then((res) => {
         setAirDrops(res);
+        checkStatus(res);
       });
   }, [name]);
   return airdrops;

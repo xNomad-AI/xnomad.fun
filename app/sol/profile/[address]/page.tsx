12590581e73586ff622bd896a15c0759e6ef3de2
JamesHealyi <healthonyi@gmail.com>
2025-02-08 13:12:56 +0800

    feat: profile header

--- app/sol/profile/[address]/page.tsx
+++ app/sol/profile/[address]/page.tsx
@@ -1,3 +1,27 @@
-export default function Page() {
-  return "Coming Soon";
+import { Container } from "@/app/layout/contianer";
+import { bungee } from "@/app/layout/font";
+import { Address } from "@/components/address";
+import clsx from "clsx";
+import Image from "next/image";
+
+export default function Page({
+  params,
+}: {
+  params: {
+    address: string;
+  };
+}) {
+  const { address } = params;
+  return (
+    <Container className='flex flex-col gap-32 w-full'>
+      <div className='flex items-center mb-32'>
+        <Image src={"/solana.png"} height={40} width={40} alt='' />
+        <Address
+          address={address}
+          enableCopy
+          className={clsx(bungee.className, "text-[40px]")}
+        />
+      </div>
+    </Container>
+  );
 }

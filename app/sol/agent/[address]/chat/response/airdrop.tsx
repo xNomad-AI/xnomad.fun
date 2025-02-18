99384bdbae069408013777d86d5aa63bb1ca2190
JamesHealyi <healthonyi@gmail.com>
2025-02-18 16:47:17 +0800

    refactor: airdrop response

---
+++ app/sol/agent/[address]/chat/response/airdrop.tsx
@@ -0,0 +1,101 @@
+import { useAirdrops } from "@/network/use-airdrops";
+import {
+  Button,
+  Card,
+  IconDiscordFilled,
+  IconTelegramFilled,
+  IconTwitterX,
+  IconWebsiteFilled,
+} from "@/primitive/components";
+import { NFT } from "@/types";
+import { useState } from "react";
+import { useChatContext } from "../store";
+
+export function Airdrop({ nft }: { nft: NFT }) {
+  const [claimedAirdrops, setClaimedAirdrops] = useState<string[]>([]);
+  const { airdrops } = useAirdrops({
+    agentAddress: nft.agentAccount.solana,
+  });
+  const { setMessage, sendMessageMutation } = useChatContext();
+  return (
+    <div className='flex flex-col gap-8 w-full'>
+      <span>Here are the airdrops you can claim:</span>
+      {airdrops?.length > 0
+        ? airdrops.map((airdrop) => (
+            <Card
+              key={airdrop?.id}
+              className='py-14 px-8 flex font-bold items-center gap-8 justify-between flex-wrap'
+            >
+              <div className='flex items-center gap-8'>
+                <img
+                  src={airdrop?.issuer?.image}
+                  height={20}
+                  width={20}
+                  className='rounded-full w-20 aspect-square'
+                />
+                <span>{airdrop?.name}</span>
+              </div>
+              <div className='flex items-center gap-8'>
+                {airdrop?.issuer.officialWebsite && (
+                  <a href={airdrop?.issuer.officialWebsite} target='_blank'>
+                    <IconWebsiteFilled className='text-size-16' />
+                  </a>
+                )}
+                {airdrop?.issuer.twitter && (
+                  <a href={airdrop?.issuer.twitter} target='_blank'>
+                    <IconTwitterX className='text-size-16' />
+                  </a>
+                )}
+                {airdrop?.issuer.telegram && (
+                  <a href={airdrop?.issuer.telegram} target='_blank'>
+                    <IconTelegramFilled className='text-size-16' />
+                  </a>
+                )}
+                {airdrop?.issuer.discord && (
+                  <a href={airdrop?.issuer.discord} target='_blank'>
+                    <IconDiscordFilled className='text-size-16' />
+                  </a>
+                )}
+                <Button
+                  disabled={
+                    !airdrop.claimable ||
+                    airdrop.claimed ||
+                    claimedAirdrops.includes(airdrop.id)
+                  }
+                  onClick={() => {
+                    const input = `Claim Airdrop of [${airdrop?.name}]`;
+                    const newMessages = [
+                      {
+                        text: input,
+                        user: "user",
+                        createdAt: Date.now(),
+                      },
+                      {
+                        text: input,
+                        user: "system",
+                        isLoading: true,
+                        createdAt: Date.now(),
+                      },
+                    ];
+                    setClaimedAirdrops((old) => {
+                      return [...old, airdrop.id];
+                    });
+                    setMessage(newMessages);
+                    sendMessageMutation.mutate({
+                      message: input,
+                    });
+                  }}
+                >
+                  {airdrop.claimable
+                    ? airdrop.claimed || claimedAirdrops.includes(airdrop.id)
+                      ? "Claimed"
+                      : "Claim"
+                    : "Not Eligible"}
+                </Button>
+              </div>
+            </Card>
+          ))
+        : "No airdrops available"}
+    </div>
+  );
+}

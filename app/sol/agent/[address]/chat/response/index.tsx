57ea1a65cdcfcfb36b3c3ce10b2dfd3fcf8eb238
JamesHealyi <healthonyi@gmail.com>
2025-02-18 19:04:55 +0800

    feat: default ai response

--- app/sol/agent/[address]/chat/response/index.tsx
+++ app/sol/agent/[address]/chat/response/index.tsx
@@ -1,6 +1,8 @@
 import { NFT } from "@/types";
 import { ContentWithUser } from "../types";
 import { Airdrop } from "./airdrop";
+import { Buy } from "./trade.tsx/buy";
+import AIWriter from "react-aiwriter";
 
 export function AiResponse({
   message,
@@ -12,8 +14,28 @@ export function AiResponse({
   switch (message.action) {
     case "airdrop":
       return <Airdrop nft={nft} />;
-
+    case "trade":
+      switch (message.tradeAction) {
+        case "buy":
+          return <Buy />;
+        // case 'sell':
+        //   return <Sell />
+        // case 'swap':
+        //   return <Swap />
+        // case 'transfer':
+        //   return <Transfer />
+        // case 'Limit-order':
+        //   return <LimitOrder />
+        // case 'copy-trade':
+        //   return <CopyTrade />
+        default:
+          return "Trade action not found";
+      }
+    case "analyze":
+    // return <Analyze />
+    case "issue-token":
+    // return <IssueToken />
     default:
-      break;
+      return <AIWriter>{message?.text}</AIWriter>;
   }
 }

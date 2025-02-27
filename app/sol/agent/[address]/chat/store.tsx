7163366bb4f96b73cd6737868442b10be0461f84
JamesHeal <790364833@qq.com>
2025-02-27 15:08:46 +0800

    fix: memory construct

--- app/sol/agent/[address]/chat/store.tsx
+++ app/sol/agent/[address]/chat/store.tsx
@@ -19,6 +19,7 @@ import { useWallet } from "@solana/wallet-adapter-react";
 import { SetState } from "ahooks/lib/createUseStorageState";
 import { stringToUuid } from "./lib/uuid";
 import { api } from "@/primitive/api";
+import { Action } from "./content/types";
 const ChatContext = createContext<{
   handleSubmitForm: (e: React.FormEvent<HTMLFormElement>) => void;
   addMessage: (
@@ -72,15 +73,46 @@ export function ChatProvider({
     }
   }, [publicKey, agentId]);
   const [messages, setMessages] = useState<ContentWithUser[]>([]);
+  const abortController = useRef<AbortController | null>(null);
   useEffect(() => {
+    // TODO: backend error, encoded room id twice, need to fix
+    const roomId = stringToUuid(userId);
+    if (abortController.current) {
+      abortController.current.abort();
+    }
+    abortController.current = new AbortController();
     api.agent
       .get<{
         agentId: string;
-        memories: ContentWithUser[];
+        memories: {
+          id: string;
+          userId: string;
+          agentId: string;
+          createdAt: number; // in ms
+          content: {
+            text: string;
+            action: string;
+            webAction: Action;
+          };
+          roomId: string;
+          unique: boolean;
+        }[];
         roomId: string;
-      }>(`/agents/${agentId}/${userId}/memories`)
+      }>(`/agents/${agentId}/${roomId}/memories`, undefined, {
+        signal: abortController.current?.signal,
+      })
       .then((res) => {
-        setMessages(res.memories);
+        setMessages(
+          res.memories
+            .sort((a, b) => a.createdAt - b.createdAt)
+            .map((msg) => ({
+              text: msg.content.text,
+              user: msg.userId === agentId ? "system" : "user",
+              createdAt: msg.createdAt,
+              id: msg.id,
+              webAction: msg.content.webAction,
+            }))
+        );
       });
   }, [agentId, userId]);
   const scrollToBottom = useMemoizedFn(() => {

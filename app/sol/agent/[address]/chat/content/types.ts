f7bd08ed2cd4e942b34ce59e12133fc4dc82a7df
JamesHeal <790364833@qq.com>
2025-02-28 16:27:34 +0800

    feat: remove copy trade

--- app/sol/agent/[address]/chat/content/types.ts
+++ app/sol/agent/[address]/chat/content/types.ts
@@ -6,7 +6,6 @@ export const tradeActions = [
   "swap",
   "transfer",
   "limit-order",
-  "copy-trade",
 ] as const;
 export type TradeAction = (typeof tradeActions)[number];
 export const actionConfigs = {
@@ -38,9 +37,5 @@ export const tradeActionConfigs = {
   },
   "limit-order": {
     title: "Limit Order",
-  },
-  "copy-trade": {
-    title: "Copy Trade",
-    disabled: true,
   },
 } as { [key in TradeAction]: { title: string; disabled?: boolean } };

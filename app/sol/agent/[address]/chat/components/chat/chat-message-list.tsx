4ef3f3317e6ebc52cab113ecd8fb45084f93b026
JamesHeal <790364833@qq.com>
2025-02-28 23:25:57 +0800

    refactor: message list

--- app/sol/agent/[address]/chat/components/chat/chat-message-list.tsx
+++ app/sol/agent/[address]/chat/components/chat/chat-message-list.tsx
@@ -1,48 +1,40 @@
 import * as React from "react";
 import { ArrowDown } from "lucide-react";
-import { useAutoScroll } from "../../hooks/useAutoScroll";
 import { useChatContext } from "../../store";
 
 interface ChatMessageListProps extends React.HTMLAttributes<HTMLDivElement> {
   smooth?: boolean;
 }
 
-const ChatMessageList = React.forwardRef<HTMLDivElement, ChatMessageListProps>(
-  ({ className, children, smooth = false, ...props }, _ref) => {
-    const { scrollRef, isAtBottom, scrollToBottom, disableAutoScroll } =
-      useAutoScroll({
-        smooth,
-        content: children,
-      });
-    const { chatBottomRef } = useChatContext();
-    return (
-      <div className='relative w-full h-full'>
-        <div
-          className={`flex flex-col w-full h-full p-4 overflow-y-auto ${className}`}
-          ref={scrollRef}
-          onWheel={disableAutoScroll}
-          onTouchMove={disableAutoScroll}
-          {...props}
-        >
-          <div className='flex flex-col gap-24'>{children}</div>
-          <div ref={chatBottomRef} className='w-full' />
-        </div>
-
-        {!isAtBottom && (
-          <button
-            onClick={() => {
-              scrollToBottom();
-            }}
-            className='absolute bottom-8 left-1/2 transform -translate-x-1/2 inline-flex rounded-full shadow-md'
-          >
-            <ArrowDown className='h-16 w-16' />
-          </button>
-        )}
+export function ChatMessageList({
+  className,
+  children,
+  ...props
+}: ChatMessageListProps) {
+  const { scrollRef, disableAutoScroll, isAtBottom, scrollToBottom } =
+    useChatContext();
+  return (
+    <div className='relative w-full h-full'>
+      <div
+        className={`flex flex-col w-full h-full p-4 overflow-y-auto ${className}`}
+        ref={scrollRef}
+        onWheel={disableAutoScroll}
+        onTouchMove={disableAutoScroll}
+        {...props}
+      >
+        <div className='flex flex-col gap-24'>{children}</div>
       </div>
-    );
-  }
-);
 
-ChatMessageList.displayName = "ChatMessageList";
-
-export { ChatMessageList };
+      {!isAtBottom && (
+        <button
+          onClick={() => {
+            scrollToBottom();
+          }}
+          className='absolute bottom-8 left-1/2 transform -translate-x-1/2 inline-flex rounded-full shadow-md'
+        >
+          <ArrowDown className='h-16 w-16' />
+        </button>
+      )}
+    </div>
+  );
+}

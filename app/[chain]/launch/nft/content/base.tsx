6fd2cf7b8f3a917298e1f117f6c3c411f1c009ac
JamesHealyi <healthonyi@gmail.com>
2025-04-07 15:55:04 +0800

    fix: typo

--- app/[chain]/launch/nft/content/base.tsx
+++ app/[chain]/launch/nft/content/base.tsx
@@ -58,7 +58,7 @@ export function Base({
       current={step}
     >
       <h1 className='text-size-24 font-bold'>Create Your Own AI Agent</h1>
-      <p className='text-size-12 text-text2'>
+      <p className='text-text2'>
         * Customize your own AI agent and launch it as an NFT!
         <br />
         * The AI agent is built on the ElizaOS framework.
--- app/layout.tsx
+++ app/layout.tsx
@@ -16,8 +16,7 @@ const ibm = IBM_Plex_Mono({
 });
 
 export const metadata: Metadata = {
-  title:
-    "xNomad.fun | The open-source smart terminal for creating and engaging with AI agents.",
+  title: "xNomad.fun | Your Ultimate AI Terminal for Crypto",
   description:
     "Discover xNomad.fun, the cutting-edge open-source platform designed to revolutionize AI agent interaction. Create, customize, and engage with AI-NFTs, powered by ElizaOS. Empower your journey in the world of decentralized AI assetization.",
 };
